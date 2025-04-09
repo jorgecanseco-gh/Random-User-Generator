be06fd9052c79c17dfb383eac2eb7639e2978af6
Jorge.C <jorgecansecoreal@gmail.com>
2025-04-09 09:43:53 +0200

    Add basic UI and style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <h1>Random User Generator</h1>
+
+    <main>
+      <button>Generate User</button>
+
+      <section className={styles.userCard}>
+        <img src="https://via.placeholder.com/150" />
+        <h2>Name: John Doe</h2>
+        <p>Email: johndoe@example.com</p>
+        <p>Location: New York, USA</p>
+      </section>
+    </main>
+  </>
+
   );
 }
 
