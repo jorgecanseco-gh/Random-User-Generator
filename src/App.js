843dda181eba5f24b44ff0932b357fdc042c13af
Jorge.C <jorgecansecoreal@gmail.com>
2025-04-09 10:35:29 +0200

    Add button function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,58 @@
 import styles from './App.module.css'
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  // Function to hold the user Data
+
+const [userData, setUserData] = useState(null);
+
+const getUserData = async () => {
+  const url = 'https://randomuser.me/api/';
+  const response = await fetch(url);
+
+  try {
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  } 
+    const data = await response.json();
+    setUserData(data.results[0]);
+  } catch (error) {
+    console.error('Error fetching user data:')
+  }
+}
+
+
+useEffect(() => {
+  getUserData();
+ 
+}, [])
+
+
+const handleClick = () => {
+  getUserData();
+}
+
+
+
   return (
     <>
         <h1>Random User Generator</h1>
 
     <main>
-      <button>Generate User</button>
+      <button
+      onClick={handleClick}
+      >
+      Generate User
+      </button>
 
+      {userData && 
       <section className={styles.userCard}>
-        <img src="https://via.placeholder.com/150" />
-        <h2>Name: John Doe</h2>
-        <p>Email: johndoe@example.com</p>
-        <p>Location: New York, USA</p>
-      </section>
+        <img src={userData.picture.large} />
+        <h2>Name: {userData.name.first + " " + userData.name.last}</h2>
+        <p>Email: {userData.email}</p>
+        <p>Location: {userData.location.city + ", " + userData.location.country}</p>
+      </section>}
     </main>
   </>
 
